8605a2369aa8d4523633672a59b8185016c15e6a
Luchadortomy <grtom16@gmail.com>
2024-06-06 00:06:19 -0700

    hola

--- src/contact/Contactpage.jsx
+++ src/contact/Contactpage.jsx
@@ -7,18 +7,16 @@ export const Contactpage = () => {
     <div className="home1">
     <div className="homeb-box">
         <h2>Contactanos</h2>
-        <p>Vamos a conectar</p>
         <div className="logo-container">
-          <Link to="https://www.instagram.com/alphacon.sanluis?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
+          <Link to="https://www.instagram.com/alphacon.sanluis" target="_blank">
             <img src="src/assets/images/insta-logo.webp" alt="" className='logo-insta'/>
           </Link>
           <Link to="https://www.facebook.com/Alphaconslrc" target="_blank">
             <img src="src/assets/images/face-logo.png" alt="" className='logo-face'/>
           </Link>
         </div>
-        <p>alphacon.sanluisㅤㅤㅤㅤAlpha Conㅤ</p>
         <h3>Correo</h3>
-        <h4>contacto@alphacon.com.mx</h4>
+        <a href="mailto:contacto@alphacon.com.mx">contacto@alphacon.com.mx !</a>
       </div>
     </div> 
   );
