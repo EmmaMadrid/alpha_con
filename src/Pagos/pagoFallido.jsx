c88e4c04cb7d56798fa957c527b267a4b4d27ed9
Luchadortomy <grtom16@gmail.com>
2024-06-06 14:12:09 -0700

    rechazadoP

--- src/Pagos/pagoFallido.jsx
+++ src/Pagos/pagoFallido.jsx
@@ -3,14 +3,16 @@ import { useLocation } from 'react-router-dom';
 import { db } from '../firebaseConfig/firebase';
 import { doc, updateDoc, getDoc } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
+import './pagoFallido.css';
 
 export const PagoFallido = () => {
 
   return (
-    <div>
+    <div className='pago-fallido-container'>
       <h1>¡Pago fallido!</h1>
+      <img src="alpha_con/src/assets/images/rechazado.png" alt="" />
       <p>Por alguna razon esta madre no funciono.</p>
-      <Link to='/'>Volver al inicio</Link>
+      <Link to='/Pago'>Atras</Link>
     </div>
   );
 };
