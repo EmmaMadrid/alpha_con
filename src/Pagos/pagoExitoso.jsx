c3df831ac5133161e79b698a6519f759709334f8
Marlonbon3 <al22304010@utslrc.edu.mx>
2024-06-06 15:08:38 -0700

    Genera nuevo boleto cada que se compra

--- src/Pagos/pagoExitoso.jsx
+++ src/Pagos/pagoExitoso.jsx
@@ -1,11 +1,21 @@
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { db } from '../firebaseConfig/firebase';
 import { doc, updateDoc, getDoc } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import './pagoExitoso.css'
 
 export const PagoExitoso = () => {
+  //Generando un nuevo boleto cada vez que se compre
+  try{
+    axios.post(`https://alpha-con-default-rtdb.firebaseio.com/boletos.json`, {
+      "BoletoMarlon": "BoletoMarlon"
+    });
+    alert("EL BOLETO FUE CREADO EXITOSAMENTE")
+  } catch (err) {
+    console.error("Error creando el boleto", err);
+  }
 
   return (
     <div className='boxxx'>
