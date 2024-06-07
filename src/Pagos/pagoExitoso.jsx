ac8bafd71797fa0b4a4fe958b50e47a6c998e6e0
Diaflosno <al22304048@utslrc.edu.mx>
2024-06-06 21:13:31 -0700

    Generando los boletos correctamente despues de pagar

--- src/Pagos/pagoExitoso.jsx
+++ src/Pagos/pagoExitoso.jsx
@@ -3,38 +3,41 @@ import './pagoExitoso.css';
 import axios from 'axios'
 
 export const PagoExitoso = () => {
-  const [ticketType, setTicketType] = useState('');
+  const [ticketType, setTicketType] = useState(null);
 
   useEffect(() => {
     const storedTicketType = localStorage.getItem('ticketType');
+    localStorage.removeItem('ticketType');
+
     if (storedTicketType) {
       setTicketType(storedTicketType);
-      localStorage.removeItem('ticketType');
-    }
-  }, []);
 
-  const boleto = {
-    estado: "ACTIVO",
-    fecha: "hoy", // Fecha actual
-    hora: "5 pm",
-    nombre: "Ricardo",
-    tipoBoleto: ticketType,
-  };
-  
-  axios.post('https://alpha-con-default-rtdb.firebaseio.com/boletos.json', boleto)
-    .then(response => {
-      console.log('Boleto agregado correctamente');
-    })
-    .catch(error => {
-      console.error('Error al agregar boleto:', error);
-    });
-  
+      const boleto = {
+        estado: "ACTIVO",
+        fechaEvento: "23 de Noviembre del 2024", // Fecha actual
+        horaEvento: "5 pm",
+        tipoBoleto: storedTicketType,
+      };
+      
+      axios.post('https://alpha-con-default-rtdb.firebaseio.com/boletos.json', boleto)
+        .then(response => {
+          console.log('Boleto agregado correctamente');
+          console.log('Datos del boleto:', boleto);
+          console.log('Clave del boleto:', response.data.name);
+        })
+        .catch(error => {
+          console.error('Error al agregar boleto:', error);
+        });
+    }
+
+  }, []);
 
   return (
     <div className='pago-exitoso'>
       <div className="boxxx">
         <h1>¡Pago Exitoso!</h1>
         {ticketType && <p>Has comprado un {ticketType}</p>}
+
       </div>
     </div>
   );
