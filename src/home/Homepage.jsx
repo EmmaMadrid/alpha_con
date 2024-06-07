a0cea5e9ee1f9744e51f1c8c660a15d913c8f4d2
Jose Moreno <wmorenoglez@gmail.com>
2024-06-07 15:54:35 -0700

    Haciendo responsive el homepage

--- src/home/Homepage.jsx
+++ src/home/Homepage.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 import './homepage.css';
-import { Link, } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 
 export const Homepage = () => {
-
   return (
+
     <div className="home-container">
-    <div className="home">
-      <div className="event_image">
-        <img src="src/assets/images/alpalogo.png" alt="alphaLogo" />
-      </div>
-      <div className="content">
-        <h2 className='que-es'
-        >¿Qué es ALPHA-CON?</h2>
-        <p style={{textAlign: "justify"}}
-        >Alpha Con es la convención de anime más grande de San Luis Río Colorado, Sonora. Celebramos el amor por el anime, manga, videojuegos, cosplay y cultura pop. Ofrecemos talleres educativos, conferencias, concursos de cosplay, exhibiciones de artistas y vendedores de mercancía exclusiva. Todos son bienvenidos en nuestra convención inclusiva y diversa. ¡Únete a nuestra comunidad apasionada de fanáticos del anime y la cultura pop en Alpha Con!</p>
-        <Link to="/pago" className="btn"
-        
-        >Comprar boletos</Link>
-      </div>
+      
+      <div className="home">
+        <div className="event_image">
+          <img src="src/assets/images/alpalogo.png" alt="alphaLogo" />
+        </div>
+        <div className="content">
+          <h2 className='que-es'>¿Qué es ALPHA-CON?</h2>
+          <p style={{textAlign: "justify"}}>
+            Alpha Con es la convención de anime más grande de San Luis Río Colorado, Sonora. Celebramos el amor por el anime, manga, videojuegos, cosplay y cultura pop. Ofrecemos talleres educativos, conferencias, concursos de cosplay, exhibiciones de artistas y vendedores de mercancía exclusiva. Todos son bienvenidos en nuestra convención inclusiva y diversa. ¡Únete a nuestra comunidad apasionada de fanáticos del anime y la cultura pop en Alpha Con!
+          </p>
+          <Link to="/pago" className="btn">Comprar boletos</Link>
+        </div>
+
+
       </div>
       <h1 className='invitados'>INVITADOS | 2024</h1>
       <div className="image-gallery">
@@ -56,12 +56,12 @@ export const Homepage = () => {
         <div className="event-details-overlay">
           <div className="event-details-content">
             <h2>FECHA DEL EVENTO</h2>
-            <p> 23 NOVIEMBRE 2024 </p>
+            <p>23 NOVIEMBRE 2024</p>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
+
 export default Homepage;
